2b8ed990ee66d56de3fcc031e908d6304c36b933
diman96 <nde96m@gmail.com>
2018-04-16 23:07:13 +0300

    mycommit
    its me

---
+++ Platform/Assets/Player_Move.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour {
+
+    public int playerSpeed=10;
+   private bool facingRight=false;
+    public int playerJumpPower=300;
+    private float moveX;
+    public Rigidbody2D playerRigidbody;
+	
+    void Start()
+    {
+        playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+        PlyerMove();
+	}
+
+     void PlyerMove()
+    {
+        //controls
+        moveX = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        //animation
+        //player direction
+        if(moveX<0.0f && facingRight==false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        //phisics
+        playerRigidbody.velocity = new Vector2(moveX * playerSpeed, playerRigidbody.velocity.y);
+    }
+
+     void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    void Jump()
+    {
+        //jumping
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up*playerJumpPower );
+    }
+}
