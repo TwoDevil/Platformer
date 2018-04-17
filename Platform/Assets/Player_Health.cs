00eb560acf5e1dc43877ea2e09da320d036e3560
SheunOleg <sasski15naryto@mail.ru>
2018-04-17 17:09:57 +0300

    Камера и смерть с сеткой тайлсета
    А что еще нам надо для счастья?

---
+++ Platform/Assets/Player_Health.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_Health : MonoBehaviour {
+
+    public bool hasDied;
+    public int health;
+	// Use this for initialization
+	void Start () {
+        hasDied = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.transform.position.y < -7)
+        {
+            hasDied = true;
+        }
+        if (hasDied == true)
+        {
+            StartCoroutine("Die");
+        }
+	}
+    IEnumerator Die()
+    {
+        SceneManager.LoadScene("Prototype_1");
+        //Debug.Log("Player has Fallen");
+        //yield return new WaitForSeconds(1);
+        //Debug.Log("Player has Die");
+        //gameObject.transform.position = new Vector3(-4, 2);
+        yield return null;
+    }
+
+}
