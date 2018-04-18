085ca07b2dc30a3d24f1bdf0ac659b89d5644032
SheunOleg <sasski15naryto@mail.ru>
2018-04-18 09:31:17 +0300

    Время и счет

---
+++ Platform/Assets/Player_Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player_Score : MonoBehaviour {
+    private float timeLeft = 120;
+    public int playerScore = 0;
+    public GameObject timeLeftUI;
+    public GameObject playerScoreUI;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        timeLeftUI.gameObject.GetComponent<Text>().text = ("Осталось времени: " + (int)timeLeft);
+        if (timeLeft < 0.1f) {
+            SceneManager.LoadScene("Prototype_1");
+        }
+	}
+}
