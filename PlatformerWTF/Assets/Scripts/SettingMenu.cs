14a8184dce4fa932a6a070e53e9edecca121f077
SheunOleg <sasski15naryto@mail.ru>
2018-04-21 19:05:34 +0300

    Меню и музон

--- PlatformerWTF/Assets/Scripts/SettingMenu.cs
+++ PlatformerWTF/Assets/Scripts/SettingMenu.cs
@@ -11,6 +11,6 @@ public class SettingMenu : MonoBehaviour {
     // Use this for initialization
     public void NewGame()
     {
-        SceneManager.LoadScene("Prototype_1");
+        SceneManager.LoadScene("Level_1");
     }
 }
