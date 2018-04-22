75e8faaa4c448402a998268bb6009ebe6431d2f5
SheunOleg <sasski15naryto@mail.ru>
2018-04-22 22:03:04 +0300

    Upgrade Menu

--- PlatformerWTF/Assets/Scripts/GlobalSetings.cs
+++ PlatformerWTF/Assets/Scripts/GlobalSetings.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Skills{
+    public int CurentSkillLevel;
+    public int MaxSkillLevel;
+    public int Value;
+    string SkillName;
+
+}
 public class GlobalSetings : MonoBehaviour {
 
     public static bool EnableGlobalFonMusic;
     public static bool EnableGlobalEffectMusic;
-
+    public static int Level;
+    public static int CurentNumber;
+    public static Skills Jump;
+    public static Skills Money;
+    public static Skills Damage;
+    public static Skills HP;
     // Use this for initialization
     void Start () {
         MuteMusic();
